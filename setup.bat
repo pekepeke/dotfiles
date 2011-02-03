f5e65637e85417feedd2f5a8ed95b1f72cdb98c2
pekepeke <pekepekesamurai+github@gmail.com>
2011-02-03 19:43:23 +0900

    fix script

--- setup.bat
+++ setup.bat
@@ -1,11 +1,13 @@
 @echo OFF
 
 cd %~dp0
+for %%i in (.vimperatorrc .vimperatorrc.js) do copy %~dp0%%i %HOME%\%%i
 for /F %%i in ('dir /A:D-S /b .*') do if not %%i == .git mklink /D %HOME%\%%i %~dp0%%i
 for /F %%i in ('dir /A:D-S /b _*') do if not %%i == .git mklink /D %HOME%\%%i %~dp0%%i
 for /F %%i in ('dir /A:A-S /b .*') do if not %%i == .git mklink    %HOME%\%%i %~dp0%%i
 for /F %%i in ('dir /A:A-S /b _*') do if not %%i == .git mklink    %HOME%\%%i %~dp0%%i
 call git submodule init
 call git submodule update
+mkdir .vim\bundle\errormarker.vim\doc
 pause
 goto :EOF
