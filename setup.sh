f50dc3e58ebd3b0ddf20dd6ae390ed45ed6c09ed
pekepeke <pekepekesamurai+github@gmail.com>
2011-02-03 22:11:13 +0900

    fix critical bug...

--- setup.sh
+++ setup.sh
@@ -18,14 +18,15 @@ main() {
     done
   fi
   for F in .?* ;do
-    if [ "$F" == "." -o "$F" == ".." -o "setup.sh" ] ; then
+    if [ "$F" == "." -o "$F" == ".." -o "$F" == "setup.sh" ] ; then
       echo skip $F
     else
       ln -s $CDIR/$F $HOME
     fi
   done
-  git submodule init
-  git submodule update
+  echo please execute under commands
+  echo git submodule init
+  echo git submodule update
 }
 
 while getopts "h:v" opt; do
