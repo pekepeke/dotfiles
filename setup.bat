c9078325205b68875c1a847d3b776e411ef54d92
pekepeke <pekepekesamurai+github@gmail.com>
2011-02-04 08:20:08 +0900

    update

--- setup.bat
+++ setup.bat
@@ -6,8 +6,9 @@ for /F %%i in ('dir /A:D-S /b .*') do if not %%i == .git mklink /D %HOME%\%%i %~
 for /F %%i in ('dir /A:D-S /b _*') do if not %%i == .git mklink /D %HOME%\%%i %~dp0%%i
 for /F %%i in ('dir /A:A-S /b .*') do if not %%i == .git mklink    %HOME%\%%i %~dp0%%i
 for /F %%i in ('dir /A:A-S /b _*') do if not %%i == .git mklink    %HOME%\%%i %~dp0%%i
-call git submodule init
-call git submodule update
+echo please execute under commands
+echo git submodule init
+echo git submodule update
 mkdir .vim\bundle\errormarker.vim\doc
 pause
 goto :EOF
