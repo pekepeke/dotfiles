981252946d845b1c06cc580ffaa73f34c717cfe5
pekepeke <pekepekesamurai+github@gmail.com>
2011-02-03 11:33:16 +0900

    setup bat for win

---
+++ setup.bat
@@ -0,0 +1,9 @@
+@echo OFF
+
+cd %~dp0
+for /F %%i in ('dir /A:D-S /b .*') do if not %%i == .git mklink /D %HOME%\%%i %~dp0%%i
+for /F %%i in ('dir /A:D-S /b _*') do if not %%i == .git mklink /D %HOME%\%%i %~dp0%%i
+for /F %%i in ('dir /A:A-S /b .*') do if not %%i == .git mklink    %HOME%\%%i %~dp0%%i
+for /F %%i in ('dir /A:A-S /b _*') do if not %%i == .git mklink    %HOME%\%%i %~dp0%%i
+pause
+goto :EOF
