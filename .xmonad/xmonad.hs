c714d6cf794b3aab9f63799aa5e95dd53a98d0fa
pekepeke <pekepekesamurai+github@gmail.com>
2012-02-03 00:38:59 +0900

    update

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,20 +1,121 @@
 -- vim:fdm=marker sw=2 ts=2 ft=haskell expandtab:
 import System.IO
+import System.Exit
+
 import XMonad
+import XMonad.Actions.GridSelect
+import XMonad.Actions.WindowGo
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Layout.Magnifier
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Grid
+import Data.Monoid
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+    -- launch a terminal
+    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+    -- launch dmenu
+    , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
+    -- launch gmrun
+    , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
+    -- close focused window
+    , ((modm .|. shiftMask, xK_c     ), kill)
+    -- Rotate through the available layout algorithms
+    , ((modm,               xK_space ), sendMessage NextLayout)
+    --  Reset the layouts on the current workspace to default
+    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+    -- Resize viewed windows to the correct size
+    , ((modm,               xK_n     ), refresh)
+    -- Move focus to the next window
+    , ((modm,               xK_Tab   ), windows W.focusDown)
+    -- Move focus to the next window
+    , ((modm,               xK_j     ), windows W.focusDown)
+    -- Move focus to the previous window
+    , ((modm,               xK_k     ), windows W.focusUp  )
+    -- Move focus to the master window
+    , ((modm,               xK_m     ), windows W.focusMaster  )
+    -- Swap the focused window and the master window
+    , ((modm,               xK_Return), windows W.swapMaster)
+    -- Swap the focused window with the next window
+    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
+    -- Swap the focused window with the previous window
+    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
+    -- Shrink the master area
+    , ((modm,               xK_h     ), sendMessage Shrink)
+    -- Expand the master area
+    , ((modm,               xK_l     ), sendMessage Expand)
+    -- Push window back into tiling
+    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
+    -- Increment the number of windows in the master area
+    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
+    -- Deincrement the number of windows in the master area
+    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
+    -- Toggle the status bar gap
+    -- Use this binding with avoidStruts from Hooks.ManageDocks.
+    -- See also the statusBar function from Hooks.DynamicLog.
+    --
+    , ((modm              , xK_b     ), sendMessage ToggleStruts)
+    -- Quit xmonad
+    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+    -- Restart xmonad
+    , ((modm              , xK_q     ), spawn "killall trayer; xmonad --recompile; xmonad --restart")
+    ]
+    ++
+    -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N
+    [((m .|. modm, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    ++
+    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+    ++
+    -- GridSelect
+    [((modm, xK_g), goToSelected defaultGSConfig)]
+
+myLayout = withBorder 1 (tiled ||| Mirror tiled ||| Grid) ||| Full
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+     -- The default number of windows in the master pane
+     nmaster = 1
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
+
 main = do
-    xmproc <- spawnPipe "xmobar"
-    xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
-            , layoutHook = avoidStruts $ layoutHook defaultConfig
-                , logHook = dynamicLogWithPP $ xmobarPP
-                { ppOutput = hPutStrLn xmproc
-                    , ppTitle = xmobarColor "green" "" . shorten 50
-                }
-            , terminal           = "gnome-terminal"
-                , borderWidth        = 2
-                , normalBorderColor  = "#333333"
-                , focusedBorderColor = "#cd8b00"
-        }
+  spawn "trayer --edge top --align right --SetDockType true --SetPartialStrut false --expand true --width 20 --transparent true --tint 0x000000 --height 12"
+  spawn "gnome-power-manager"
+  spawn "gnome-volume-control-applet"
+  spawn "nm-applet --sm-disable"
+  spawn "bluetooth-applet"
+  spawn "nautilus --no-desktop -n"
+  -- spawn "dropbox start"
+  xmproc <- spawnPipe "xmobar"
+  xmonad $ defaultConfig
+    { 
+    logHook = dynamicLogWithPP $ xmobarPP
+      { ppOutput = hPutStrLn xmproc
+      , ppTitle = xmobarColor "green" "" . shorten 50
+      }
+    , manageHook = manageDocks <+> manageHook defaultConfig
+    -- , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , layoutHook         = myLayout
+    , terminal           = "urxvt"
+    , borderWidth        = 2
+    , normalBorderColor  = "#dddddd"
+    , focusedBorderColor = "#ff0000"
+    , workspaces         = ["1","2","3","4","5","6","7","8","9"]
+     -- key bindings
+    , keys               = myKeys
+    , mouseBindings      = myMouseBindings
+    }
