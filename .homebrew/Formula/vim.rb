5d3ccf9e339494b7afcab937d03274ac0435b2b7
pekepeke <pekepekesamurai+github@gmail.com>
2012-08-08 22:00:45 +0900

    update

--- .homebrew/Formula/vim.rb
+++ .homebrew/Formula/vim.rb
@@ -6,25 +6,28 @@ require 'formula'
 class Vim <Formula
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  url 'https://vim.googlecode.com/hg/', :revision => 'c45a38bd18a9'
-  version '7.3.237'
+  url 'https://vim.googlecode.com/hg/', :revision => '52247eb61511'
+  version '7.3.622'
   homepage 'http://www.vim.org/'
 
+  # head 'https://code.google.com/p/vim/'
   head 'https://vim.googlecode.com/hg/'
 
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
-                          "--enable-gui=no",
+                          "--disable-gui",
                           "--without-x",
+                          "--disable-gpm",
                           "--disable-nls",
                           "--enable-multibyte",
                           "--with-tlib=ncurses",
                           "--enable-perlinterp",
                           "--enable-pythoninterp",
+                          # "--enable-python3interp",
                           "--enable-rubyinterp",
                           "--with-features=huge"
-    system "make"
+    # system "make"
     system "make install"
   end
 end
