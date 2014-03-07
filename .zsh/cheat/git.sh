6c72e884fe6400df4377b4c826a6772c1bf15b9a
pekepeke <pekepekesamurai+github@gmail.com>
2014-03-07 23:59:50 +0900

    add cheat

--- .zsh/cheat/git.sh
+++ .zsh/cheat/git.sh
@@ -6,6 +6,12 @@ git reflog --pretty=full              # reflog でログ参照
 git reflog --stat                     # reflog でログ参照
 git reset HEAD^                       # 最新のコミットをstaging状態に戻せる(--soft つきでもOK)
 
+git revert f60f24d                    # revert コミットを作成
+git revert -m 1 f60f24d               # merge コミットを revert(parent number を指定)
+git show f60f24d                      # parent number を確認(049d32b = 1, ebbcb6a = 2)
+commit f60f24d34845fba4e038b3e165f74973b3a19580
+Merge: 049d32b ebbcb6a
+
 git push origin <branch>:<remote branch>              # ローカルのブランチをリモートにpush
 git checkout -b <local branch> origin/<remote branch> # リモートのブランチをとってくる
 git reset HEAD hoge/hoge.c; git checkout hoge/hoge.c  # git rmからの復活
