a65ffed013193fe6ba147e3e3ef1ed169ca4ef06
pekepeke <pekepekesamurai+github@gmail.com>
2014-07-15 21:56:58 +0900

    add cheat. update tm2 config.

---
+++ .zsh/cheat/curl.sh
@@ -0,0 +1,6 @@
+# basic
+curl -u user:pass http://hoge.com
+# webdav upload
+curl --upload somefile https://example.com/foo/
+# user agent
+curl -A ua http://hoge.com
---
+++ .zsh/cheat/zip.sh
@@ -0,0 +1,6 @@
+# ディレクトリ内を再帰的に圧縮
+zip -r file.zip dir/
+# パスワード付きで圧縮
+zip -e file.zip filename
+# stored 圧縮
+zip -0 file.zip filename
