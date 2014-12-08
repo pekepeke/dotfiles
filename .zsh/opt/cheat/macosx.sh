28c5f882b28224987bdca96946bf7ec8f06cc3cb
pekepeke <pekepekesamurai+github@gmail.com>
2014-12-08 20:05:28 +0900

    update peco source. update cheat. update ghq config.

--- .zsh/opt/cheat/macosx.sh
+++ .zsh/opt/cheat/macosx.sh
@@ -1,3 +1,22 @@
-# Finderのツールバーに追加
+## Finderのツールバーに追加
 ⌘キーを押下しながらDrag & Drop
+
+## caffeinate でスリープを防ぐ
+caffeinate brew update &
+caffeinate open -a Safari
+caffeinate -t 3600 &
+
+## pkgutil でパッケージファイルを展開
+pkgutil --expand test.pkg ~/Desktop/test/
+
+## 同じアプリを複数開く
+open -n /Applications/Safari.app
+
+## App Store を使わずに OS X をアップデート
+# アップデートの有無を確認
+$ sudo softwareupdate -l
+
+# 利用可能なアップデートを全てインストール
+$ sudo softwareupdate -i -a
+
 
