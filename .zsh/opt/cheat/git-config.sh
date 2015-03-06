6bbf3a76a7d22e42b81ae356e4b3aae35613e486
pekepeke <pekepekesamurai+github@gmail.com>
2015-03-06 21:30:49 +0900

    add alias

--- .zsh/opt/cheat/git-config.sh
+++ .zsh/opt/cheat/git-config.sh
@@ -1,2 +1,16 @@
 # locally gitignore
 vim .git/info/exclude
+
+# minimum config
+git config --global push.default upstream
+git config --global pull.rebase true
+git config --global alias.st status
+git config --global alias.stt status -uno
+git config --global alias.ss status -sb
+git config --global alias.co checkout
+git config --global alias.ls ls-files
+git config --global alias.ci commit
+git config --global alias.ca commit --amend
+git config --global alias.br branch
+
+
