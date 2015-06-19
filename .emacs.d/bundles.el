6a760c862a02456939d6930898b5247635bf01e3
pekepeke <pekepekesamurai+github@gmail.com>
2015-06-19 09:58:46 +0900

    update emaca bundle

--- .emacs.d/bundles.el
+++ .emacs.d/bundles.el
@@ -1,16 +1,34 @@
 (el-get-bundle popwin)
 (el-get-bundle direx)
+(el-get-bundle auto-complete)
 (el-get-bundle ac-emmet)
 (el-get-bundle ac-etags)
 (el-get-bundle ac-inf-ruby)
 (el-get-bundle ac-ispell)
 (el-get-bundle ac-js2)
 (el-get-bundle ac-math)
-(el-get-bundle color-moccur)
 (el-get-bundle company)
+
+(el-get-bundle color-theme)
+(el-get-bundle color-theme-molokai)
+(el-get-bundle color-moccur)
+(el-get-bundle grep-edit)
 (el-get-bundle recentf-ext)
 (el-get-bundle quickrun)
+(el-get-bundle smartchr)
 (el-get-bundle yasnippet)
+(el-get-bundle yasnippet-config)
+(el-get-bundle projectile)
+(el-get-bundle undo-tree)
+
+;;; evil
+(el-get-bundle evil)
+(el-get-bundle evil-numbers evil-nerd-commenter evil-leader)
+(el-get-bundle evil-surround)
+(el-get-bundle evil-plugins)
+(el-get-bundle evil-rails)
+
+;;; helm
 (el-get-bundle helm)
 (el-get-bundle helm-descbinds)
 (el-get-bundle helm-projectile helm-dired-recent-dirs)
@@ -19,13 +37,8 @@
 (el-get-bundle helm-git-grep helm-git-files)
 (el-get-bundle helm-css-scss helm-dash)
 (el-get-bundle ac-helm)
-(el-get-bundle projectile)
-(el-get-bundle undo-tree)
-(el-get-bundle evil)
-(el-get-bundle evil-numbers evil-nerd-commenter evil-leader)
-;; yasnippet-config
-;; evil-surround
-;; mode-compile
+
+(el-get-bundle mode-compile)
 (el-get-bundle flycheck)
 (el-get-bundle expand-region)
 (el-get-bundle ido-ubiquitous)
@@ -35,6 +48,7 @@
 (el-get-bundle rainbow-delimiters)
 (el-get-bundle diminish)
 (el-get-bundle exec-path-from-shell)
+(el-get-bundle editorconfig)
 (el-get-bundle ack-and-a-half)
 (el-get-bundle dired+)
 (el-get-bundle hlinum)
@@ -42,43 +56,69 @@
 (el-get-bundle point-undo)
 (el-get-bundle sequential-command)
 (el-get-bundle zlc)
-;; -- lang
-(el-get-bundle color-theme)
 (el-get-bundle highlight-parentheses)
+(el-get-bundle historyf)
+
+;; -- lang
 (el-get-bundle bash-completion)
-(el-get-bundle web-mode)
-(el-get-bundle mmm-mode)
+
+;;; perl
 (el-get-bundle plenv)
 (el-get-bundle perl-completion)
+(el-get-bundle tmt-mode)
+
+;;; python
+(el-get-bundle python-mode)
 (el-get-bundle jedi)
+
+;;; ruby
 (el-get-bundle ruby-mode)
 (el-get-bundle rinari)
+(el-get-bundle rcodetools)
 (el-get-bundle rhtml-mode)
 (el-get-bundle rsense)
 (el-get-bundle inf-ruby)
 (el-get-bundle ruby-block)
 (el-get-bundle ruby-end)
 (el-get-bundle rbenv)
+
+;;; php
+(el-get-bundle php-mode-improved)
+(el-get-bundle php-completion)
+(el-get-bundle php-eldoc)
+
 ;; -- html5
+(el-get-bundle web-mode)
+(el-get-bundle mmm-mode)
 (el-get-bundle haml-mode)
 (el-get-bundle js2-mode)
 (el-get-bundle emmet-mode)
 (el-get-bundle tern)
 (el-get-bundle coffee-mode)
 (el-get-bundle sass-mode)
-(el-get-bundle clojure-mode)
-(el-get-bundle cider)
-(el-get-bundle haskell-mode)
+(el-get-bundle auto-complete-css)
+(el-get-bundle auto-complete-emacs-lisp)
+
+;;; go
 (el-get-bundle go-mode)
 (el-get-bundle go-autocomplete)
 (el-get-bundle go-eldoc)
+
+(el-get-bundle clojure-mode)
+(el-get-bundle cider)
+(el-get-bundle haskell-mode)
+(el-get-bundle applescript-mode)
+
+;;; texts
 (el-get-bundle textile-mode)
 (el-get-bundle markdown-mode)
 (el-get-bundle yaml-mode)
+(el-get-bundle rst-mode)
+
+;;; configs
 (el-get-bundle apache-mode)
-(el-get-bundle applescript-mode)
 ;; run-test
-(el-get-bundle google-translate)
+
 ;; -- git
 (el-get-bundle magit)
 (el-get-bundle gitconfig-mode)
@@ -89,23 +129,6 @@
 (el-get-bundle gh)
 (el-get-bundle gist)
 
-(el-get-bundle auto-complete)
-(el-get-bundle auto-complete-css)
-(el-get-bundle auto-complete-emacs-lisp)
-(el-get-bundle yasnippet-config)
-(el-get-bundle grep-edit)
-(el-get-bundle smartchr)
-(el-get-bundle color-theme-molokai)
-(el-get-bundle evil-surround)
-(el-get-bundle evil-rails)
-(el-get-bundle evil-plugins)
-(el-get-bundle mode-compile)
-(el-get-bundle historyf)
-(el-get-bundle tmt-mode)
-(el-get-bundle rcodetools)
-(el-get-bundle python-mode)
-(el-get-bundle php-mode-improved)
-(el-get-bundle php-completion)
-(el-get-bundle php-eldoc)
-(el-get-bundle rst-mode)
+
+(el-get-bundle google-translate)
 (el-get-bundle run-test)
