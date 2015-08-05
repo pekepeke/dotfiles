e7b59e6a7e17bb413db75d8a30c2eb6fd4581c08
pekepeke <pekepekesamurai+github@gmail.com>
2015-08-06 08:24:24 +0900

    add google-chrome

--- bin/install/source/apt-get_prepare.sh
+++ bin/install/source/apt-get_prepare.sh
@@ -14,6 +14,11 @@ EOM
 # ppa:nilarimogard/webupd8 # for albert
 }
 
+add_chrome_repository() {
+  sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+}
+
 apt_keys() {
   cat <<EOM
 EOM
@@ -22,6 +27,7 @@ EOM
 for p in $(repositories); do
   sudo add-apt-repository -y $p
 done
+add_chrome_repository
 
 for url in $(apt_keys); do
   curl -L ${url} | sudo apt-key add -
