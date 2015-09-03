27cad139dfd71a2fe3872d1e71d6369948b65f63
pekepeke <pekepekesamurai+github@gmail.com>
2015-09-03 10:51:21 +0900

    update ctags

---
+++ bin/install/source/packages/universal-ctags.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+usage() {
+  prg_name=`basename $0`
+  cat <<EOM
+  Usage: $prg_name [-h]
+EOM
+  exit 1
+}
+
+main() {
+  git clone https://github.com/universal-ctags/ctags.git /tmp/ctags --depth=1
+  cd /tmp/ctags
+  autoreconf -vfi
+  ./configure --enable-iconv
+  make
+  cp ctags ~/.bin/
+}
+
+OPTIND_OLD=$OPTIND
+OPTIND=1
+while getopts "hvs:" opt; do
+  case $opt in
+    h)
+      usage ;;
+    v) ;;
+    s)
+      #$OPTARG
+      ;;
+  esac
+done
+shift `expr $OPTIND - 1`
+OPTIND=$OPTIND_OLD
+if [ $OPT_ERROR ]; then
+  usage
+fi
+
+main "$@"
+
