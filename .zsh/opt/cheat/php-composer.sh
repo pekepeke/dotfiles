ae4f5ae778f13601c22ca0abf5d752f44fbed07b
pekepeke <pekepekesamurai+github@gmail.com>
2015-09-15 19:13:22 +0900

    add cheat

---
+++ .zsh/opt/cheat/php-composer.sh
@@ -0,0 +1,11 @@
+## composer.json に package 追加
+
+```
+composer require phpunit/phpunit:3.7.*       # requireに追加
+composer require phpunit/phpunit:3.7.* --dev # require-devに追加
+```
+
+## self-upgrade
+```
+composer self-upgrade
+```
