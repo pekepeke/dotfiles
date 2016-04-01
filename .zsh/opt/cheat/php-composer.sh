b3c6e8a809f3777c5bbe5a6bf2ad2cb52a31fabb
pekepeke <pekepekesamurai+github@gmail.com>
2016-04-01 20:05:41 +0900

    update

--- .zsh/opt/cheat/php-composer.sh
+++ .zsh/opt/cheat/php-composer.sh
@@ -5,6 +5,13 @@ composer require phpunit/phpunit:3.7.*       # requireに追加
 composer require phpunit/phpunit:3.7.* --dev # require-devに追加
 ```
 
+## remove
+
+```
+composer remove jenssegers/mongodb
+composer remove jenssegers/mongodb --update-with-dependencies
+```
+
 ## self-upgrade
 ```
 composer self-upgrade
