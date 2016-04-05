e1f43f31b037b788693fddd7ade9b9b83af38f9d
pekepeke <pekepekesamurai+github@gmail.com>
2016-04-05 19:31:27 +0900

    update download scripts

--- bin/.lib/github_go.sh
+++ bin/.lib/github_go.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
 
+download_release_package() {
+  PACKAGE="$1"
+  VER="$2"
+
+  if [ -z "$PACKAGE" -o -z "$VER" ]; then
+    echo "invalid arguments: $*" 1>&2
+    return 1
+  fi
+
+  EXT=$(detect_ext zip)
+  URL=$(get_release_binary_url "$PACKAGE" "$VER" "$EXT")
+
+  cd /tmp/
+  local LOCAL_PACKAGE=/tmp/$(basename $PACKAGE).$EXT
+  if [ $EXT = zip ] ; then
+    curl -L "${URL}" -o $LOCAL_PACKAGE
+    unzip $LOCAL_PACKAGE
+  else
+    curl -L "${URL}" -o $LOCAL_PACKAGE
+    tar zxvf $LOCAL_PACKAGE
+  fi
+}
+
+detect_ext() {
+  EXT="$1"
+  local PLATFORM=$(uname -s | tr [A-Z] [a-z])
+  [ -n "$EXT" -a "$PLATFORM" = "linux" ] && EXT=tar.gz
+  echo $EXT
+}
+
 get_release_binary_url() {
   PACKAGE="$1"
   VER="$2"
   EXT="$3"
 
-  local PLATFORM=$(uname -i)
+  local ARCH=$(uname -i)
   local PLATFORM=$(uname -s | tr [A-Z] [a-z])
 
   if [ "$ARCH" = "x86_64" ] ; then
