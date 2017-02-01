684c3ffd333a2a68a57ceeec60fa9e9f8af43d36
pekepeke <pekepekesamurai+github@gmail.com>
2017-02-01 22:07:50 +0900

    update

--- bin/install/source/packages/phpchecker.sh
+++ bin/install/source/packages/phpchecker.sh
@@ -12,17 +12,21 @@ EOM
 
 main() {
   declare -A bins
+  bzip2_installed=`php -i | grep -e 'bzip2' | grep enabled | wc -l`
   bins["phpunit"]=https://phar.phpunit.de/phpunit.phar
   bins["phpunit-skelgen"]=https://phar.phpunit.de/phpunit-skelgen.phar
   bins["php-cs-fixer"]=https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v2.0.0/php-cs-fixer.phar
   bins["phpcs"]=https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
   bins["phploc"]=https://phar.phpunit.de/phploc.phar
-  bins["phpmd"]=http://static.phpmd.org/php/latest/phpmd.phar
   bins["phpdcd"]=https://phar.phpunit.de/phpdcd.phar
   bins["phpcpd"]=https://phar.phpunit.de/phpcpd.phar
+  bins["phpsa"]=https://github.com/ovr/phpsa/releases/download/0.6.2/phpsa.phar
   bins["php-nag"]=https://github.com/algo13/php-nag/releases/download/0.0.1-bata2/phpnag.phar
   bins["phpmig"]=https://github.com/monque/PHP-Migration/releases/download/v0.2.2/phpmig.phar
   # bins[""]=
+  if [ $bzip2_installed -eq 0 ]; then
+    bins["phpmd"]=http://static.phpmd.org/php/latest/phpmd.phar
+  fi
   [ ! -e ~/.bin/ ] && mkdir -p ~/.bin/
   for bin in "${!bins[@]}"; do
     [ "$opt_force" = 1 -a -e ~/.bin/$bin ] && rm ~/.bin/$bin
@@ -30,6 +34,10 @@ main() {
       && [ ! -x ~/.bin/$bin ] && chmod +x ~/.bin/$bin \
       && echo "installed $bin"
   done
+
+  if [ $bzip2_installed -eq 0 ]; then
+    echo "not found : bz2 extension..."
+  fi
 }
 
 OPTIND_OLD=$OPTIND
