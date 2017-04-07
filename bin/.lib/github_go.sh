6d8070acdf04c0f98ace303d36e14640f4830b1c
pekepeke <pekepekesamurai+github@gmail.com>
2017-04-07 20:25:11 +0900

    fixes command

--- bin/.lib/github_go.sh
+++ bin/.lib/github_go.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
+get_latest_version() {
+  PACKAGE="$1"
+  T=/tmp/github_latest_$(echo $PACKAGE | base64)
+  if [ ! -e $T ]; then
+    curl https://api.github.com/repos/$PACKAGE/releases/latest > $T
+  fi
+  cat $T | python -c 'import sys,json; print json.load(sys.stdin)["tag_name"];'
+}
+
 download_release_package() {
   PACKAGE="$1"
   VER="$2"
+  PACKAGE_BASENAME="$3"
+  EXT="$4"
 
   if [ -z "$PACKAGE" -o -z "$VER" ]; then
     echo "invalid arguments: $*" 1>&2
     return 1
   fi
 
-  EXT=$(detect_ext zip)
-  URL=$(get_release_binary_url "$PACKAGE" "$VER" "$EXT")
+  [ -z "$PACKAGE_BASENAME" ] && PACKAGE_BASENAME=$(basename $PACKAGE)
+  [ -z "$EXT" ] && EXT=$(detect_ext zip)
+  URL=$(get_release_binary_url "$PACKAGE" "$VER" $PACKAGE_BASENAME "$EXT")
 
   cd /tmp/
   local LOCAL_PACKAGE=/tmp/$(basename $PACKAGE).$EXT
+  echo $URL
   if [ $EXT = zip ] ; then
     curl -L "${URL}" -o $LOCAL_PACKAGE
     unzip $LOCAL_PACKAGE
@@ -33,7 +46,8 @@ detect_ext() {
 get_release_binary_url() {
   PACKAGE="$1"
   VER="$2"
-  EXT="$3"
+  PACKAGE_BASENAME="$3"
+  EXT="$4"
 
   local ARCH=$(uname -i)
   local PLATFORM=$(uname -s | tr [A-Z] [a-z])
@@ -43,8 +57,10 @@ get_release_binary_url() {
   else
     ARCH=386
   fi
-  [ -n "$EXT" -a "$PLATFORM" = "linux" ] && EXT=tar.gz
+  [ -z "$EXT" -a "$PLATFORM" = "linux" ] && EXT=tar.gz
   [ -n "$EXT" ] && EXT=".${EXT}"
+  [ -z "$PACKAGE_BASENAME" ] && PACKAGE_BASENAME=$(basename $PACKAGE)
 
-  echo "https://github.com/${PACKAGE}/releases/download/v${VER}/$(basename $PACKAGE)_${PLATFORM}_${ARCH}${EXT}"
+
+  echo "https://github.com/${PACKAGE}/releases/download/${VER}/${PACKAGE_BASENAME}_${PLATFORM}_${ARCH}${EXT}"
 }
