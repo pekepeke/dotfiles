e8dee6b58f330c0977068a5fdbe1425bd30beb3f
pekepekesamurai <pekepekesamurai+github@gmail.com>
2019-09-09 20:17:51 +0900

    update

--- .tmux.d/ver.sh
+++ .tmux.d/ver.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-perl -e "exit 0 if `tmux -V | sed 's/^tmux\s*//'` $* ; exit 1;"
+# perl -e "exit 0 if `tmux -V | sed 's/^tmux\s*//'` $* ; exit 1;"
+perl -e "my \$v = \`tmux -V\`; \$v =~ s/^tmux\\s*|\\D*\$//g; exit 0 if (\$v * 1) $*; exit 1"
 exit $?
