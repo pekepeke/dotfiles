1b4a4c99e62d2fd5282defa3367449359102ba04
pekepekesamurai <pekepekesamurai+github@gmail.com>
2020-02-04 19:50:37 +0900

    remove unused code

--- bin/.opt/mitmproxy/docker-functions.sh
+++ bin/.opt/mitmproxy/docker-functions.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-opt_port=8080
-
 is_int() {
   [ "$1" -eq 0 ] 2> /dev/null
   test $? -ne 2
@@ -12,11 +10,4 @@ err_p() {
   echo "$*" 1>&2
 }
 
-usage() {
-  prg_name=`basename $0`
-  cat <<EOM
-  Usage: $prg_name [-h]
-EOM
-  exit 1
-}
 
