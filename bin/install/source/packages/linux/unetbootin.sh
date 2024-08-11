9609c6f19e6db9320fc33350f4d599314b52b3a9
pekepeke <pekepekesamurai+github@gmail.com>
2024-08-11 16:58:42 +0900

    add unetbootin

---
+++ bin/install/source/packages/linux/unetbootin.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+
+usage() {
+  prg_name=`basename $0`
+  cat <<EOM
+  Usage: $prg_name [-h]
+EOM
+  exit 1
+}
+
+main() {
+  # AUTHOR=tldr-pages
+  PG=unetbootin
+  INSTALL_DIR=~/.local/bin
+  # if [ -e "$INSTALL_DIR/$PG" ]; then
+  #   echo "already installed: $INSTALL_DIR/$PG"
+  #   return 0
+  # fi
+  ARCH=`uname -m`
+  DL_URL=''
+  VER=702
+  case $ARCH in
+    x86_64)
+      DL_URL='https://github.com/unetbootin/unetbootin/releases/download/$VER/unetbootin-linux64-$VER.bin'
+      ;;
+    i386|i686)
+      DL_URL='https://github.com/unetbootin/unetbootin/releases/download/$VER/unetbootin-linux-$VER.bin'
+      ;;
+    *)
+      ;;
+  esac
+  if [ "$DL_URL" != "" ]; then
+    curl -Lo /tmp/$PG "$DL_URL"
+    chmod +x /tmp/$PG $INSTALL_DIR
+  else
+    echo "url not found: arch=$ARCH" 1>&2
+
+  fi
+  # DL_URL="$(curl -s https://api.github.com/repos/$AUTHOR/$PG/releases/latest \
+# | grep "browser_download_url.*$ARCH-.*linux-musl" \
+# | cut -d : -f 2,3 \
+# | tr -d \")"
+  # if [ "$DL_URL" = "" ]; then
+  #   echo "url not found" 1>&2
+  #   return 1
+  # fi
+  # curl -LO $DL_URL
+  # tar xvf $(basename $DL_URL) -C /tmp/
+  # mv /tmp/$PG $INSTALL_DIR
+  # # mv /tmp/$(basename $DL_URL .tar.gz)/$PG $INSTALL_DIR
+  # rm -f $(basename $DL_URL)
+}
+
+OPTIND_OLD=$OPTIND
+OPTIND=1
+while getopts "hvs:" opt; do
+  case $opt in
+    h)
+      usage ;;
+    v) ;;
+    s)
+      #$OPTARG
+      ;;
+  esac
+done
+shift `expr $OPTIND - 1`
+OPTIND=$OPTIND_OLD
+if [ $OPT_ERROR ]; then
+  usage
+fi
+
+main "$@"
+
+
